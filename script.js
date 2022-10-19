85ad815ed5b7a604e5738045c59711e5d95b6372
Michael OBrien <michaelob@outlook.com.au>
2022-10-20 07:22:10 +1000

    Added C and F functionality

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ import { getGeocode } from './getGeocode.js';
 
 const weatherApp = (() => {
   let key = '14924e1bfbc96cdfc9467c223bc62cbf';
-  let units = 'metric';
   let search = document.querySelector('.search');
   let weatherObj;
   let currentDate = Date().slice(0, 15);
   let currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit',});
-  let lat;
-  let lon;
   let loading = document.querySelector('.loading');
+  let units = 'metric'
+  let celc = document.querySelector('.celcius');
+  let fahren = document.querySelector('.fahrenheit');
+  let location = document.querySelector('#location');
 
   let getCurrentWeather = async (location) => {
     try {
       loading.style.display = 'block'
       let geodata = await getGeocode(location, key);
-      lat = geodata.lat;
-      lon = geodata.lon;
-      let getWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=${units}`)
+      let getWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geodata.lat}&lon=${geodata.lon}&appid=${key}&units=${units}`)
       let response = await getWeather.json();
       weatherObj = {
         name: response.name,
@@ -38,11 +37,12 @@ const weatherApp = (() => {
           .join(' ');
         },
         weatherIcon: response.weather[0].icon,
-        wind: () => {
+        windKmph: () => {
           let speed = response.wind.speed * 3.6; // convert to km/h
           speed = (Math.round(speed * 10) / 10); // round to 1 decimal place
           return speed;
         },
+        windMph: response.wind.speed,
         windDir: response.wind.deg
       }
       loading.style.display = 'none'
@@ -52,29 +52,55 @@ const weatherApp = (() => {
     }
   }
 
-  let populateWeather = async () => {
-    let location = document.querySelector('#location').value;
-    location = location.replace(/ /g,"_");
-    await getCurrentWeather(location);
-    console.log(weatherObj)
-    document.querySelector('.cityName').textContent = weatherObj.name;
-    document.querySelector('.date').textContent = `${currentDate}, ${currentTime}`
-    document.querySelector('.weatherDesc').textContent = weatherObj.weatherDesc();
-    document.querySelector('.temp').textContent = `${weatherObj.temp}°C`;
-    document.querySelector('.feelsLike').textContent = `Feels like ${weatherObj.feelsLike}°C`;
-    document.querySelector('#weatherIcon').src = `https://openweathermap.org/img/w/${weatherObj.weatherIcon}.png`;
-
-    document.querySelector('.minTemp').textContent = `Min - ${weatherObj.tempMin}°C`; 
-    document.querySelector('.maxTemp').textContent = `Max - ${weatherObj.tempMax}°C`;
-    document.querySelector('.humidity').textContent = `Humidity - ${weatherObj.humidity}%`;
-    document.querySelector('.wind').textContent = `Wind - ${weatherObj.wind()}km/h`;
+  let populateWeather = async (location) => {
+      try {
+        let unitOfMeasure;
+        units === 'metric' ? unitOfMeasure = 'C' : unitOfMeasure = 'F';
+        location = location.replace(/ /g,"_"); // convert all spaces to underscore
+        await getCurrentWeather(location);
+        console.log(weatherObj)
+        document.querySelector('.cityName').textContent = weatherObj.name;
+        document.querySelector('.date').textContent = `${currentDate}, ${currentTime}`
+        document.querySelector('.weatherDesc').textContent = weatherObj.weatherDesc();
+        document.querySelector('.temp').textContent = `${weatherObj.temp}°${unitOfMeasure}`;
+        document.querySelector('.feelsLike').textContent = `Feels like ${weatherObj.feelsLike}°${unitOfMeasure}`;
+        document.querySelector('#weatherIcon').src = `https://openweathermap.org/img/w/${weatherObj.weatherIcon}.png`;
+        document.querySelector('.minTemp').textContent = `Min - ${weatherObj.tempMin}°${unitOfMeasure}`; 
+        document.querySelector('.maxTemp').textContent = `Max - ${weatherObj.tempMax}°${unitOfMeasure}`;
+        document.querySelector('.humidity').textContent = `Humidity - ${weatherObj.humidity}%`;
+        document.querySelector('.wind').textContent = units === 'metric' ? `Wind - ${weatherObj.windKmph()} km/h` : `Wind - ${weatherObj.windMph} mph`
+      } catch (err) {
+        console.error(err);
+      }
   }
 
   document.querySelector('#location').addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-      populateWeather();
+    if (e.key === 'Enter' && location.value !== '') {
+      populateWeather(location.value);
     }
-  })
+  });
   
-  search.addEventListener('click', populateWeather);
+  search.addEventListener('click', () => {
+    if (location.value !== '') {
+      populateWeather(location.value);
+    }
+  });
+
+  celc.addEventListener('click', () => {
+    if (location.value !== '') {
+      celc.style.color = 'var(--mainTextColour)';
+      fahren.style.color = 'lightgrey';
+      units = 'metric';
+      populateWeather(location.value);
+    }
+  });
+
+  fahren.addEventListener('click', () => {
+    if (location.value !== '') {
+      celc.style.color = 'lightgrey';
+      fahren.style.color = 'var(--mainTextColour)';
+      units = 'imperial';
+      populateWeather(location.value);
+    }
+  });
 })();
